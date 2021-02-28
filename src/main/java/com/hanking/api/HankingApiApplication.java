b21f4470dc419f9028c729410ec022900f815940
bergson <bergson.silva.rodrigues@gmail.com>
2021-02-28 11:29:54 -0300

    Adicionado a estrutura do projeto

---
+++ src/main/java/com/hanking/api/HankingApiApplication.java
@@ -0,0 +1,13 @@
+package com.hanking.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HankingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HankingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanking/api/HankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanking.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
